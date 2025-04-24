6eb9f917165f9cf20c805dd77549789f7fc148ce
Isabella Passarelli <isabellapassarelli025@gmail.com>
2025-04-23 22:23:32 -0300

    Created scanner code

--- tcp-port-scanner/scanner/tcp_scanner.py
+++ tcp-port-scanner/scanner/tcp_scanner.py
@@ -1,1 +1,18 @@
 #Função para fazer a varredura
+import socket
+
+def scan_ports(host, start_port, end_port):
+    print(f'Scannin {host} from port {start_port} to {end_port}...')
+
+    for port in range(start_port, end_port+1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+
+        result = s.connect_ex((host, port))
+        if result == 0:
+            print(f"[+] Port {port} open")
+        s.close()
+
+if __name__ == "__main__":
+    target = input("Enter the IP address or domain to scan: ")
+    scan_ports(target, 1, 100)
